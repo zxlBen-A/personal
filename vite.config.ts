356d4275f5682c60d2266c221f86889766e30e92
zxlben <1752533779@qq.com>
2022-08-28 23:17:16 +0800

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,39 @@
+import {defineConfig} from 'vite'
+import vue from '@vitejs/plugin-vue'
+import AutoImport from "unplugin-auto-import/vite"
+import Components from 'unplugin-vue-components/vite'
+import {ElementPlusResolver} from 'unplugin-vue-components/resolvers'
+
+const path = require('path')
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
+        },
+    },
+    plugins: [ //插件
+        vue({
+            reactivityTransform: true,//使用ref创建的属性不需要使用value了
+        }),
+        AutoImport({
+            imports: ["vue", "vue-router"], // 自动导入vue和vue-router相关函数
+            dts: "src/auto-import.d.ts", // 生成 `auto-import.d.ts` 全局声明
+            resolvers: [ElementPlusResolver()],//element 按需引入
+        }),
+        Components({
+            resolvers: [ElementPlusResolver()],//element 按需引入
+        }),
+    ],
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: '@import "@/styles/main.scss";'
+            }
+        }
+    },
+    server: {
+        host: '10.101.196.121',
+    },
+})
